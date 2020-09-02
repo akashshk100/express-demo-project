a2f414638247fad2f9d848dc585b8c633914e2ac
akash shukla <shuklaji.2227@gmail.com>
2020-09-02 16:25:26 +0530

    web interface added

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
-const { get } = require('http')
+const request = require('request')
+const { json } = require('express')
 
 const app = express()
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'public/templates/views'))
-hbs.registerPartials(path.join(__dirname, 'public/templates/partials'))
+app.set('views', path.join(__dirname, 'templates/views'))
+hbs.registerPartials(path.join(__dirname, 'templates/partials'))
 
 
 app.get('/', (req, res) => {
@@ -35,9 +36,28 @@ app.get('/weather', (req, res) => {
             error: "please provide address"
         })
     }
-    res.send({
-        temperature: 23,
-        city: req.query.address
+    var weather_data
+    address = req.query.address
+    weather_api_url = 'http://api.weatherstack.com/current?access_key=108fe864921471410c42ffa7cff3e3e8&query='+address
+    request.get({url: weather_api_url, json: true}, (error, response) => {
+        if(error){
+            res.send({error: 'Error while connecting please try again later'})
+        }
+        else if(response.body.error){
+            res.send({error: response.body.error.info})
+        }
+        else{
+            // console.log(response.body.current)
+            weather_data = {
+                location: response.body.location.name+', '+response.body.location.country,
+                temperature: response.body.current.temperature,
+                weather_description: response.body.current.weather_descriptions[0],
+                humidity: response.body.current.humidity,
+                wind_speed: response.body.current.wind_speed,
+                weather_icon: response.body.current.weather_icons[0]
+            }
+            res.send(weather_data)
+        }
     })
 })  
 
